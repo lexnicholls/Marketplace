00f0a2306f0714143f14089de420f2c8b4699f20
Alexander Nicholls <nicholls.aom@gmail.com>
2018-09-14 23:23:52 -0500

    ProductoEntity Clase encargada de crear la entidad proveedor.

---
+++ src/main/java/edu/konrad/marketplace/entities/ProveedorEntity.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.konrad.marketplace.entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Alexander Nicholls
+ */
+@Entity(name = "Proveedor")
+public class ProveedorEntity implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long idProveedor;
+
+    @Column(name = "nombre_proveedor")
+    private String nombreProveedor;
+
+    private String nit;
+
+    public Long getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(Long idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public String getNombreProveedor() {
+        return nombreProveedor;
+    }
+
+    public void setNombreProveedor(String nombreProveedor) {
+        this.nombreProveedor = nombreProveedor;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public ProveedorEntity() {
+
+    }
+}
